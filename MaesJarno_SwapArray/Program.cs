1322be1ec34d38a26e53287f435832b3450d8935
JARNO MAES <JARNO.MAES@student.intecbrussel.be>
2020-11-19 09:45:35 +0100

    Beter

--- MaesJarno_SwapArray/Program.cs
+++ MaesJarno_SwapArray/Program.cs
@@ -13,26 +13,42 @@ namespace MaesJarno_SwapArray
             int[] A = new int[6] { 1, 2, 3, 4, 5, 6 };
             int[] B = new int[6] { 6, 5, 4, 3, 2, 1 };
 
+            Console.WriteLine("Before the swap");
+            Console.ReadLine();
+            
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+
+            Console.ReadLine();
+
+            for (int i = 0; i < B.Length; i++)
+            {
+                Console.WriteLine(B[i]);
+            }
+
             for (int i = 0; i < B.Length; i++)
             {
                 int temp = A[i];
                 A[i] = B[i];
                 B[i] = temp;
             }
+            Console.ReadLine();
+            Console.WriteLine("After the swap");
+            Console.ReadLine();
 
-            for (int i = 0; i < B.Length; i++)
-            {
+            for (int i = 0; i < A.Length; i++)
+            {               
                 Console.WriteLine(A[i]);
-
             }
             
             Console.ReadLine();          
             
-            for (int i = 0; i < A.Length; i++)
+            for (int i = 0; i < B.Length; i++)
             {
                 Console.WriteLine(B[i]);
             }
-        }
-        
+        }   
     }
 }
